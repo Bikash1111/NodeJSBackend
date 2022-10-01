d2434ebbfe6824599aeb52988398dd80f8ac8dfd
bikash.maharana <bikash.maharana@infosys.com>
2022-10-01 19:53:15 +0530

    format changes

--- PracticeProj/index.js
+++ PracticeProj/index.js
@@ -1,37 +1,38 @@
 // import module from different file
 
 /*
-const app = require('./app')
-const arr = [5,3,5,6,7,87,3,1];
+        const app = require('./app')
+        const arr = [5,3,5,6,7,87,3,1];
 
-let result  =  arr.filter((data) =>{
-    return data>5
-})
+        let result  =  arr.filter((data) =>{
+            return data>5
+        })
 
-console.log(app)
-console.log(result)
+        console.log(app)
+        console.log(result)
 
 */
 
-/*
-console.log("path: ",__dirname)
-console.log("file name: ",__filename)
 
-/*
+        console.log("path: ",__dirname)
+        console.log("file name: ",__filename)
+
 
 //create file system
 /*
-const fs = require('fs')
-fs.writeFileSync("NewFile","Bikash Maharana")
+        const fs = require('fs')
+        fs.writeFileSync("NewFile","Bikash Maharana")
 
 */
 
 //create server
+/*
+        const http = require('http');
 
-const http = require('http');
+        http.createServer((req,res)=>{
+            res.write("<h1>This is first Title </h1>")
+            res.end();
+        }
+        ).listen(4500);
 
-http.createServer((req,res)=>{
-    res.write("<h1>This is first Title </h1>")
-    res.end();
-}
-).listen(4500);
+*/
